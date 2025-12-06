03d3289ee6ab29b69d0f5f67e63ad1ceeb9ebbb0
Ablasse Kingcaid-Ouedraogo <ablassecode@outlook.com>
2025-12-06 14:41:57 -0600

    Update .config/craftzdog/dotfiles-public/.config/nvim/lazy-lock.json Update .config/craftzdog/dotfiles-public/.config/nvim/lazyvim.json Update .config/craftzdog/dotfiles-public/.config/nvim/lua/plugins/editor.lua Update .config/fish/config.fish Update .config/fish/fish_variables

--- dot_config/craftzdog/dotfiles-public/dot_config/nvim/lua/plugins/editor.lua
+++ dot_config/craftzdog/dotfiles-public/dot_config/nvim/lua/plugins/editor.lua
@@ -253,7 +253,7 @@ return {
 	{
 		"nvim-focus/focus.nvim",
 		version = false,
-		require("focus").setup({
+		opts = {
 			enable = true, -- Enable module
 			commands = true, -- Create Focus commands
 			autoresize = {
@@ -262,22 +262,22 @@ return {
 				height = 0, -- Force height for the focused window
 				minwidth = 0, -- Force minimum width for the unfocused window
 				minheight = 0, -- Force minimum height for the unfocused window
-				focusedwindow_minwidth = 0, --Force minimum width for the focused window
+				focusedwindow_minwidth = 90, --Force minimum width for the focused window
 				focusedwindow_minheight = 0, --Force minimum height for the focused window
 				height_quickfix = 10, -- Set the height of quickfix panel
 			},
 			split = {
-				bufnew = false, -- Create blank buffer for new split windows
+				bufnew = true, -- Create blank buffer for new split windows
 				tmux = false, -- Create tmux splits instead of neovim splits
 			},
 			ui = {
-				number = true, -- Display line numbers in the focussed window only
+				number = false, -- Display line numbers in the focussed window only
 				relativenumber = false, -- Display relative line numbers in the focussed window only
 				hybridnumber = false, -- Display hybrid line numbers in the focussed window only
 				absolutenumber_unfocussed = false, -- Preserve absolute numbers in the unfocussed windows
 
 				cursorline = true, -- Display a cursorline in the focussed window only
-				cursorcolumn = true, -- Display cursorcolumn in the focussed window only
+				cursorcolumn = false, -- Display cursorcolumn in the focussed window only
 				colorcolumn = {
 					enable = false, -- Display colorcolumn in the foccused window only
 					list = "+1", -- Set the comma-saperated list for the colorcolumn
@@ -285,6 +285,6 @@ return {
 				signcolumn = true, -- Display signcolumn in the focussed window only
 				winhighlight = false, -- Auto highlighting for focussed/unfocussed windows
 			},
-		}),
+		},
 	},
 }
--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -12,6 +12,14 @@ abbr -a -g fzfrm "fzf | xargs -I {} rm {}"
 abbr -a -g nvimm "NVIM_APPNAME=mimikun/dotfiles/dot_config/nvim nvim"
 abbr -a -g nvidem "NVIM_APPNAME=mimikun/dotfiles/dot_config/nvim neovide"
 
+if test -d "~/.config/mimikun"
+    gh repo clone mimikun/dotfiles ~/.config/mimikun/dotfiles
+end
+
+if test -d "~/.config/craftzdog/dotfiles-public/"
+    gh repo clone craftzdog/dotfiles-public ~/.config/craftzdog/dotfiles-public
+end
+
 #source ~/.config/fish/conf.d/_tide_init.fish
 mise activate fish | source
 zoxide init fish | source

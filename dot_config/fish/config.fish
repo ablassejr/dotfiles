c860da565a88de3ef8b80dd043fb0be8fa032360
Ablasse Kingcaid-Ouedraogo <ablassecode@outlook.com>
2026-05-03 08:51:34 -0500

    Trim dotfile bloat

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,31 +1,30 @@
-abbr -a -g la "exa --long --tree --level 3"
-abbr -a -g bashconfig "micro ~/.bashrc"
-abbr -a -g zshconfig "micro ~/.zshrc"
-abbr -a -g lmost "ls -la | most"
-abbr -a -g mkexec "sudo -S chmod +x"
-abbr -a -g jupyter-tui "source ~/.local/share/jupyter-tui/.venv/bin/activate && python ~/.local/share/jupyter-tui/main.py"
-abbr -a -g savesession "hyprsession --mode save-and-exit && shutdown now"
+abbr -a -g la "eza --long --tree --level 3"
+abbr -a -g ll "ls -lh"
+abbr -a -g gs "git status"
+abbr -a -g ga "git add"
+abbr -a -g gc "git commit"
+abbr -a -g gp "git push"
+abbr -a -g ce "chezmoi edit"
+abbr -a -g ca "chezmoi apply"
 abbr -a -g clr clear
-abbr -a -g nvimc "NVIM_APPNAME=craftzdog/dotfiles-public/.config/nvim nvim"
-abbr -a -g nvidec "NVIM_APPNAME=craftzdog/dotfiles-public/.config/nvim neovide"
-abbr -a -g fzfrm "fzf | xargs -I {} rm {}"
-abbr -a -g nvimm "NVIM_APPNAME=mimikun/dot_config/nvim nvim"
-abbr -a -g nvidem "NVIM_APPNAME=mimikun/dotfiles/dot_config/nvim neovide"
 abbr -a -g nvimp "NVIM_APPNAME=nvim-personal nvim"
 abbr -a -g nvidep "NVIM_APPNAME=nvim-personal neovide"
-abbr -a -g releasesudo "faillock --user $(whoami) --reset"
 abbr -a -g nvide neovide
+abbr -a -g tm task-master
+abbr -a -g taskmaster task-master
+abbr -a -g oc opencode
 
-if test -d "~/.config/mimikun"
-    gh repo clone mimikun/dotfiles ~/.config/mimikun/dotfiles
+fish_add_path --path $HOME/.local/bin
+fish_add_path --path $HOME/.cargo/bin
+fish_add_path --path $HOME/.bun/bin
+fish_add_path --path $HOME/.opencode/bin
+fish_add_path --path /opt/homebrew/bin /opt/homebrew/sbin
+fish_add_path --append --path /Library/TeX/texbin
+
+if command -q mise
+    mise activate fish | source
 end
 
-if test -d "~/.config/craftzdog/dotfiles-public/"
-    gh repo clone craftzdog/dotfiles-public ~/.config/craftzdog/dotfiles-public
+if command -q zoxide
+    zoxide init fish | source
 end
-
-#source ~/.config/fish/conf.d/_tide_init.fish
-mise activate fish | source
-zoxide init fish | source
-
-command clear
